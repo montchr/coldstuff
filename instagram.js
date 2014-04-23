9de3217153204828272b87ffa256fab98c6f6c7f
Chris Montgomery <chris@montchr.io>
2014-04-23 15:40:12 -0400

    file output

--- instagram.js
+++ instagram.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 // dotenv
 // for the super secrets
 var dotenv = require('dotenv');
@@ -22,15 +24,90 @@ var lat = 39.952247,
     lng = -75.163894;
 
 /**
+ * Add leading zero to integer if it's less than 10
+ * @param {integer} n An integer
+ */
+function addZero (n) {
+  return n < 10 ? '0' + n : '' + n;
+}
+
+
+/**
+ * Write file
+ * @param  {string} filename
+ * @param  {mixed} data
+ */
+var writeFile = function (filename, data) {
+  fs.writeFile(filename, data, 'utf8', function(e) {
+    if (e) throw e;
+    console.log(filename + ' written!');
+  });
+};
+
+
+/**
+ * mkdir wrapper function
+ * @param  {string} d The directory to create
+ * @param {integer} p The directory permissions
+ */
+var mkdir = function (d,p) {
+  fs.mkdir(d, p, function(e){
+    if (fs.readdir(d, function (e,f) {
+      if (e) throw e;
+    })) return;
+    if (e) throw e;
+  });
+};
+
+
+/**
+ * rmdir wrapper function
+ * @param  {string} d The directory to remove
+ */
+var rmdir = function (d) {
+  fs.rmdir(d, function(e){
+    if (e) throw e;
+  });
+};
+
+
+/**
  * Returns an array of Instagram photos for each day
  *
- * @param {object} top Type of top 25 days to use. Use either 'HotDays'
- * or 'ColdDays'.
+ * @param {object} topDays Type of top 25 days to use. Use either 'hot'
+ * or 'cold'.
  */
-var Photos = function (top) {
-  for (var i = 0; i < top.length; i++) {
-    var temp = top[i].temp_rounded,
-        minTimestamp = top[i].timestamp,
-        maxTimestamp = minTimestamp + 86400;
+var Photos = function (hc) {
+
+  if (hc === 'hot') {
+    topDays = HotDays;
+  } else if (hc === 'cold') {
+    topDays = ColdDays;
   }
+
+  for (var i = 0; i < topDays.length; i++) {
+    var minTimestamp = topDays[i].timestamp,
+        maxTimestamp = minTimestamp + 86400;
+
+    var rank = topDays[i].rank;
+
+    // time and date
+    var time = new Date(minTimestamp * 1000),
+        year = time.getUTCFullYear(),
+        month = addZero(time.getUTCMonth()),
+        day = addZero(time.getUTCDate()),
+        date = year + '-' + month + '-' + day;
+
+    // output
+    var filename = './responses/instagram-' + hc + '-' + addZero(rank) + '-' + date + '.json',
+        buffer = minTimestamp + ' ' + maxTimestamp;
+
+    writeFile(filename, buffer);
+  }
+
 };
+
+// rmdir('./responses');
+mkdir('./responses', 0777);
+Photos('hot');
+Photos('cold');
